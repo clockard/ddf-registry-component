729dd7ecdbd552bdd362d055a51ece3734d496bc
Scott Tustison <scott.a.tustison@lmco.com>
2013-09-11 13:16:21 -0700

    Updated admin ui example plugin

---
+++ ui/admin/ui-admin-example/src/main/webapp/app/js/hawt-io-tab.js
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) Codice Foundation
+ *
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ *
+ **/
+
+//This .js file just needs to be referenced by the blueprint config to load the tab in hawtio
+//We don't need to actually load this JavaScript in our web app (and we shouldn't)
+
+// create our angular module and tell angular what route(s) it will handle
+
+var simplePlugin = angular.module('admin_plugin', ['hawtioCore'])
+    .config(function($routeProvider) {
+        $routeProvider.
+        when('/admin_plugin', {
+            templateUrl: '../hawtio/ui-admin-example/admin.html'
+        });
+})
+
+
+simplePlugin.run(function(workspace, viewRegistry, layoutFull) {
+
+// tell the app to use the full layout, also could use layoutTree
+// to get the JMX tree or provide a URL to a custom layout
+viewRegistry["admin_plugin"] = layoutFull;
+
+// Set up top-level link to our plugin
+workspace.topLevelTabs.push({
+    content: "Admin",
+    title: "Admin plugin loaded dynamically",
+    isValid: function() { return true; },
+    href: function() { return "#/admin_plugin"; },
+    isActive: function() { return workspace.isLinkActive("admin_plugin"); }
+});
+
+});
+
+// tell the hawtio plugin loader about our plugin so it can be
+// bootstrapped with the rest of angular
+hawtioPluginLoader.addModule('admin_plugin');
