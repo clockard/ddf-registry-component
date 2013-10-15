f3804ab39e82637c8c71dcbab305f76b3197f5f6
Scott Tustison <scott.a.tustison@lmco.com>
2013-10-15 15:34:42 -0700

    DDF-185 removed example files

--- ui/admin/ui-admin-federated-source/src/main/webapp/app/client/models/example.model.js
+++
@@ -1,8 +0,0 @@
-var Results={}
-    Results.Model = Backbone.Model.extend({
-       // I don't do anything in here because I don't care.
-    });
-    Results.Collection = Backbone.Collection.extend({
-        model: Results.Model,
-        url : "../json/search.json"
-    });
